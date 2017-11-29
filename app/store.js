d2200d3ff4eafe84c4b64b5eca357a828135fe12
Marcin Kornek <marcinkornek@gmail.com>
2017-11-29 07:32:23 +0100

    Use remote-redux-devtools

--- app/store.js
+++ app/store.js
@@ -1,7 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'remote-redux-devtools';
 import thunk from 'redux-thunk';
 import reducers from 'DetoxTest/app/reducers/index';
 
-const store = applyMiddleware(thunk)(createStore)(reducers);
+const store = composeWithDevTools(applyMiddleware(thunk))(createStore)(
+  reducers,
+);
 
 export default store;
